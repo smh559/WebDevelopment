2c9cdd1301a1dec61218e285566fbbacf40c1219
smh559 <102369522+smh559@users.noreply.github.com>
2024-06-17 19:27:44 -0400

    Add files via upload

--- lab11_DOM/script.js
+++ lab11_DOM/script.js
@@ -24,3 +24,39 @@ s4.innerHTML ="OFF by <b><em>midnight</em></b>"
 let spans = document.querySelectorAll("#special span")
 // assign the class name to one element in the list 'span'
 spans[1].className ="changefontfamily"
+
+// setAttribute method
+// find the first <h1> element and we are going to set the class attribute 'title' to it
+let title = document.querySelector("h1")
+title.setAttribute("class", "styletitle")
+
+//remove a class name from an element
+//remove the class 'info' from <div class="info">
+// let divinfo = document.querySelector(".info")
+// divinfo.classList.remove("")
+
+let divmoreinfo = document.querySelector(".moreinfo")
+divmoreinfo.classList.remove("info")
+
+//append a text after <div id="special">
+let divspecial = document.querySelector("#special")
+divspecial.append("NEW INFORMATION")
+
+// appendChild to add a new <li> to <ul class ="todo">
+// step 1) create <li> element
+let newitem = document.createElement("li")
+// step 2) give content to the next <li>
+let newlist = document.createTextNode("Work on Website build")
+// join the newlist into newitem.
+newitem.appendChild(newlist)
+// step 3) append as child the newitem into the document
+// append to <ul class="todo">
+let todolist = document.querySelector(".todo")
+todolist.appendChild(newitem)
+
+// remove an element using removeChild method
+todolist.removeChild(todolist.children[1])
+
+// remove an element using remove method
+// select the element that you want to remove and use the method remove
+todolist.children[1].remove()
